58ec8e2ccd7b7d8549aad34a14a107c564291b8b
xiayulu <harolddfinchh@gmail.com>
2024-12-24 22:46:49 +0800

    ready

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./your_program.sh tokenize test.lox
--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,6 @@ int main(int argc, char *argv[]) {
 
     if (command == "tokenize") {
         std::string file_contents = read_file_contents(argv[2]);
-        
-        // Uncomment this block to pass the first stage
         if (!file_contents.empty()) {
             std::cerr << "Scanner not implemented" << std::endl;
             return 1;
